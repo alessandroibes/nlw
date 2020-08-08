8a1b5a7ec8686d6fcc7967baa9a44681f2bbdfa7
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2020-08-08 12:47:07 -0300

    Commit da versão 1.0 finalizada

---
+++ aulas/server/src/utils/convertHourToMinutes.ts
@@ -0,0 +1,6 @@
+export default function converHourToMinutes(time: string) {
+    // hh:mm
+    const [hour, minutes] = time.split(':').map(Number);
+    const timeInMinutes = (hour * 60) + minutes;
+    return timeInMinutes;
+}
